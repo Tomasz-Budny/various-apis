95405d7500307f769137666cb4cf493302a9e7a1
Tomasz-Budny <s208246@sggw.edu.pl>
2023-06-14 12:22:27 +0200

    Add memes view functionality

--- src/app/views/memes-view/memes/memes.component.ts
+++ src/app/views/memes-view/memes/memes.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MemeService } from 'src/app/services/meme.service';
+import { Observable } from 'rxjs';
+import { MemeModel } from 'src/app/models/meme.model';
 
 @Component({
   selector: 'memes',
@@ -10,4 +13,23 @@ import { CommonModule } from '@angular/common';
 })
 export class MemesComponent {
 
+  meme$: Observable<MemeModel>
+  categories = [
+    'videogamememes', 'politicalmemes', 'memes',
+    'videogamememes', 'politicalmemes', 'memes',
+    'videogamememes', 'politicalmemes', 'memes',
+    'videogamememes', 'politicalmemes', 'memes',
+  ];
+
+  constructor(
+    public memeService: MemeService
+  ) {}
+
+  ngOnInit() {
+    this.meme$ = this.memeService.getRandomMeme('memes')
+  }
+
+  getMemeOfSpecifficCategory(category: string) {
+    this.meme$ = this.memeService.getRandomMeme(category);
+  }
 }
