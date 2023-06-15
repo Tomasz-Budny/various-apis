fd51078cf52a5ff2aebb2593f0c3bf1b75cf31c0
Tomasz-Budny <s208246@sggw.edu.pl>
2023-06-15 13:15:09 +0200

    localize content

--- src/app/views/footer/footer.component.ts
+++ src/app/views/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslocoRootModule } from 'src/app/transloco-root.module';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslocoRootModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
--- src/app/views/memes-view/memes-view.component.ts
+++ src/app/views/memes-view/memes-view.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MemesComponent } from './memes/memes.component';
+import { TranslocoRootModule } from 'src/app/transloco-root.module';
 
 @Component({
   selector: 'memes-view',
   standalone: true,
-  imports: [CommonModule, MemesComponent],
+  imports: [CommonModule, MemesComponent, TranslocoRootModule],
   templateUrl: './memes-view.component.html',
   styleUrls: ['./memes-view.component.scss']
 })
