29b4f9d555cd1926cd660f7abfefbc3cfae2d459
Tomasz-Budny <s208246@sggw.edu.pl>
2023-06-17 12:05:23 +0200

    Add locale to cookie-policy

--- src/app/views/cookie-policy-pop-up/cookie-policy-pop-up.component.ts
+++ src/app/views/cookie-policy-pop-up/cookie-policy-pop-up.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslocoRootModule } from 'src/app/transloco-root.module';
 
 @Component({
   selector: 'cookie-policy-pop-up',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslocoRootModule],
   templateUrl: './cookie-policy-pop-up.component.html',
   styleUrls: ['./cookie-policy-pop-up.component.scss']
 })
